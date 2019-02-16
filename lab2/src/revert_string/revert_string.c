f7ac381abbb3eb362694ecea47abdb2ca8f7547d
e1four15f <tesskyrim000@gmail.com>
2019-02-16 17:58:42 +0000

    Completed lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,22 @@
 
 void RevertString(char *str)
 {
-	// your code here
+    int len = 0;
+    char* end = str - 1;
+    while (str[len] != '\0')
+    {
+        len++;
+        end++;
+    }    
+    
+    char* begin = str;
+
+    for (int c = 0; c < len/2; c++, begin++, end--)
+    {        
+        char temp = *end;
+        *end   = *begin;
+        *begin = temp;
+    }    
 }
+
 
